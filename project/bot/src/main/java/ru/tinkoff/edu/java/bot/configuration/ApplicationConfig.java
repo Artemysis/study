243f312decb7c7915156526ecde7d3f0a5d7e131
coolt <coolt@redmibool>
2023-03-19 15:06:30 +0500

    Задание 1 Этап 4. часть работы выполнена остался ключ

--- project/bot/src/main/java/ru/tinkoff/edu/java/bot/configuration/ApplicationConfig.java
+++ project/bot/src/main/java/ru/tinkoff/edu/java/bot/configuration/ApplicationConfig.java
@@ -1,4 +1,8 @@
+package ru.tinkoff.edu.java.bot.configuration;
+
 import jakarta.validation.constraints.NotNull;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
--- project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
+++ project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
@@ -1,3 +1,10 @@
+package ru.tinkoff.edu.java.scrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import ru.tinkoff.edu.java.scrapper.configuration.ApplicationConfig;
+
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationConfig.class)
 public class ScrapperApplication {
