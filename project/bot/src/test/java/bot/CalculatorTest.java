efc79744ef5a7deb379a9dbcbc6c63db3872d9c4
coolt <coolt@redmibool>
2023-04-02 23:44:36 +0500

    Заработали unit тесты написал простой тест для проверки

---
+++ project/bot/src/test/java/bot/CalculatorTest.java
@@ -0,0 +1,11 @@
+package bot;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    @Test
+    public void testAddition() {
+        assertEquals(5, 5);
+    }
+}
