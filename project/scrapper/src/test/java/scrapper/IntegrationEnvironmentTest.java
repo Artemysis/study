aa8098edfc5ea81f4a57b374de85ec26af266b81
coolt <coolt@host.docker.internal>
2023-04-16 19:50:22 +0500

    практика 5 выполенена.

--- project/scrapper/src/test/java/scrapper/IntegrationEnvironmentTest.java
+++ project/scrapper/src/test/java/scrapper/IntegrationEnvironmentTest.java
@@ -1,13 +1,24 @@
 package scrapper;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Testcontainers
 public class IntegrationEnvironmentTest {
+  @Container
+  private static final PostgreSQLContainer<?> POSTGRES_CONTAINER =
+      new PostgreSQLContainer<>(DockerImageName.parse("postgres:14-alpine"))
+          .withDatabaseName("scrapper")
+          .withUsername("Artem")
+          .withPassword("12345");
 
-    @Test
-    public void testContainerStartup() {
-        IntegrationEnvironment.startContainer();
-
-    }
+  @Test
+  void testContainerStarted() {
+      assertTrue(POSTGRES_CONTAINER.isRunning());
+  }
 }
